4bdd8c29bc4b99047c515f8a0b7bc75a3d9f25b3
MustafaRaheel26 <mustafaraheel26@gmail.com>
2025-01-02 20:46:54 +0500

    seventh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "../context/UserContext";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <Navbar />
       <Component {...pageProps} />
     </UserProvider>
   );
