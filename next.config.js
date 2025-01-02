b2d35d3e0a680d328e8cafa82fd3b1d37ee8e952
MustafaRaheel26 <mustafaraheel26@gmail.com>
2025-01-02 20:09:10 +0500

    second

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['your-image-domain.com', 'example.com'], // Replace with actual image domains
-    },
-  };
-  
-  module.exports = nextConfig;
-  
